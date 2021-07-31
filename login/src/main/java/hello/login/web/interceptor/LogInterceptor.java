89c22ff8f74251fc1f12ecbdf72e5d11f8749ae2
bigduk88 <hangdeuk@naver.com>
2021-07-31 10:23:54 +0900

    [2021.07.31] 스프링 인터셉터 - 요청로그

---
+++ login/src/main/java/hello/login/web/interceptor/LogInterceptor.java
@@ -0,0 +1,10 @@
+package hello.login.web.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Slf4j
+public class LogInterceptor implements HandlerInterceptor {
+
+
+}
